3bfecd56b3266a7fddc6059165fd1d6a5673d6e8
Asma <a.laouy2064@uca.ac.ma>
2025-11-11 17:45:13 +0100

    first commit

---
+++ JavaApplication1/src/Connexion/Connexion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connexion;
+
+/**
+ *
+ * @author X1 YOGA
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Connexion {
+    private static Connexion instance;
+    private final Connection conn;
+    private static final String URL  = "jdbc:mysql://localhost:3306/atelier";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    private Connexion() throws SQLException {
+        conn = DriverManager.getConnection(URL, USER, PASS);
+        conn.setAutoCommit(true);
+    }
+
+    public static synchronized Connexion getInstance() throws SQLException {
+        if (instance == null || instance.conn.isClosed()) {
+            instance = new Connexion();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+   
+}
+
